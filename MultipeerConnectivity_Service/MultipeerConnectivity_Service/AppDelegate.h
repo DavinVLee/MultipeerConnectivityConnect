f86f29259390fa74755eea9f08cef8cb66ef59dc
DavinVLee <970969044@qq.com>
2017-03-29 16:51:46 +0800

    first Commit

---
+++ MultipeerConnectivity_Service/MultipeerConnectivity_Service/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipeerConnectivity_Service
+//
+//  Created by 李翔 on 2017/3/29.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultipeerConnectivity_Service/MultipeerConnectivity_Service/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultipeerConnectivity_Service
+//
+//  Created by 李翔 on 2017/3/29.
+//  Copyright © 2017年 李翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
